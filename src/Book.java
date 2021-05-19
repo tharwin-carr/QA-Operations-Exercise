8f9ecb4c5658318e4ca9416fc3ab1b6f2dcd641a
Tharwin <tharwin.carr08@gmail.com>
2021-05-19 11:52:25 -0700

    TC: added book example

---
+++ src/Book.java
@@ -0,0 +1,30 @@
+
+public class Book {
+
+	// Attributes
+
+	public String title;
+	public String author;
+	public String genre;
+	public String edition;
+	public int pageCount;
+	public boolean isOpen;
+
+	// Constructor
+
+	public Book(String title, int pageCount, String edition) {
+		this.title = title;
+		this.pageCount = pageCount;
+		this.edition = edition;
+	}
+
+	// Methods
+
+	public void open() {
+		this.isOpen = true;
+	}
+	
+	public void closed() {
+		this.isOpen = false;
+	}
+}
---
+++ src/Runner.java
@@ -0,0 +1,11 @@
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Book exampleBook = new Book("The Green Pass", 250, "First Edit.");
+		
+		System.out.println(exampleBook.title);
+
+	}
+
+}
